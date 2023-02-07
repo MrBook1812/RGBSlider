b680fa830ffb9ab685d7b776991cf51997929f09
Kirill Kniga <kiruha_95@icloud.com>
2023-02-07 22:29:36 +0300

    ok

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redCounter: UITextField!
+    @IBOutlet var greenCounter: UITextField!
+    @IBOutlet var blueCounter: UITextField!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func redSliderAction() {
+        rgbView.backgroundColor = .red.withAlphaComponent(CGFloat(redSlider.value))
+        redCounter.text = redSlider.value.formatted()
+    }
+    
+    @IBAction func greenSliderAction() {
+        rgbView.backgroundColor = .green.withAlphaComponent(CGFloat(redSlider.value))
+        greenCounter.text = greenSlider.value.formatted()
+    }
+    
+    @IBAction func blueSliderAction() {
+        rgbView.backgroundColor = .blue.withAlphaComponent(CGFloat(redSlider.value))
+        blueCounter.text = blueSlider.value.formatted()
+    }
 }
 
